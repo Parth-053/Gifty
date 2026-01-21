265e26ae007d37cc437d5229a1dec266764103a1
Parth-053 <u24cs028@coed.svnit.ac.in>
2026-01-21 19:24:04 +0530

    auth controller changes

--- server/src/controllers/auth/auth.controller.js
+++ server/src/controllers/auth/auth.controller.js
@@ -5,8 +5,8 @@ import {
 } from "../../services/auth.service.js";
 import { Seller } from "../../models/seller.model.js"; 
 import { asyncHandler } from "../../utils/asyncHandler.js";
-import { ApiResponse } from "../../utils/apiResponse.js";
-import { ApiError } from "../../utils/apiError.js";
+import { ApiResponse } from "../../utils/ApiResponse.js";
+import { ApiError } from "../../utils/ApiError.js";
 import { 
   accessTokenCookieOptions, 
   refreshTokenCookieOptions 
